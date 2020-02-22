64501233e4b0385194d0105a4397f9a4e4c9897a
veera <chandrasekharcvv@gmail.com>
2020-02-21 17:40:34 -0800

    configure eslint

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -1,12 +1,10 @@
 import expect from 'expect';
-import {Shop, Item} from '../src/gilded_rose';
+import { Shop, Item } from '../src/gilded_rose';
 
-describe("Gilded Rose", () => {
-
-  it("should foo", () => {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+describe('Gilded Rose', () => {
+  it('should foo', () => {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(items[0].name).toBe('foo');
   });
-
 });

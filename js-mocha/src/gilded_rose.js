aa31280cdc1d7fe2bbba149e867f9b9e7f5b6537
cheedep <chandrasekharcvv@gmail.com>
2020-02-25 22:42:50 -0800

    refactor code to process items by name

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-continue */
 /* eslint-disable no-param-reassign */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
@@ -7,57 +8,36 @@ export default class Shop {
     this.items = items;
   }
 
-  _updateBrie(item){
-    item.quality = Math.min(50, item.quality+1);
-  }
-
   updateQuality() {
     const backstagePasses = 'Backstage passes to a TAFKAL80ETC concert';
     const agedBrie = 'Aged Brie';
     const sulfuras = 'Sulfuras, Hand of Ragnaros';
-    for (let i = 0; i < this.items.length; i+=1) {
+    for (let i = 0; i < this.items.length; i += 1) {
       const item = this.items[i];
-      if (item.name !== agedBrie && item.name !== backstagePasses) {
-        if (item.quality > 0) {
-          if (item.name !== sulfuras) {
-            item.quality -= 1;
-          }
+      if (item.name === agedBrie) {
+        item.quality = Math.min(50, item.quality + (item.sellIn > 0 ? 1 : 2));
+        item.sellIn -= 1;
+        continue;
+      } else if (item.name === backstagePasses) {
+        item.quality = Math.min(50, item.quality + 1);
+        if (item.sellIn < 11) {
+          item.quality = Math.min(50, item.quality + 1);
         }
-      } else if (item.quality < 50) {
-        item.quality += 1;
-        if (item.name === backstagePasses) {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality += 1;
-            }
-          }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality += 1;
-            }
-          }
+        if (item.sellIn < 6) {
+          item.quality = Math.min(50, item.quality + 1);
         }
-      }
-      if (item.name !== sulfuras) {
+        item.sellIn -= 1;
+        if (item.sellIn < 0) {
+          item.quality = 0;
+        }
+        continue;
+      } else if (item.name === sulfuras) {
+        continue;
+      } else {
+        item.quality = Math.max(0, item.quality - (item.sellIn > 0 ? 1 : 2));
         item.sellIn -= 1;
       }
-      if (item.sellIn < 0) {
-        if (item.name !== agedBrie) {
-          if (item.name !== backstagePasses) {
-            if (item.quality > 0) {
-              if (item.name !== sulfuras) {
-                item.quality -= 1;
-              }
-            }
-          } else {
-            item.quality = 0;
-          }
-        } else if (item.quality < 50) {
-          item.quality += 1;
-        }
-      }
     }
-
     return this.items;
   }
 }
--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -5,20 +5,25 @@ import Item from '../src/item';
 
 describe('Gilded Rose', () => {
   function readDayToItemsFromFile() {
-    const lines = fs.readFileSync('test/stdout.gr', 'utf-8')
+    const lines = fs
+      .readFileSync('test/stdout.gr', 'utf-8')
       .split('\n')
       .filter(Boolean);
     const numOfDays = lines.length / 11;
     const days = new Array(numOfDays).fill(0).map((v, i) => i);
     const dayToItems = days.reduce((map, x) => {
-      const itemStr = lines.slice((x * 11) + 2, (x * 11) + 11);
-      const items = itemStr.map((s) => {
+      const itemStr = lines.slice(x * 11 + 2, x * 11 + 11);
+      const items = itemStr.map(s => {
         const y = s.split(',');
-        return new Item(y.slice(0, y.length - 2).join(), parseInt(y[y.length - 2], 10), parseInt(y[y.length - 1], 10));
+        return new Item(
+          y.slice(0, y.length - 2).join(),
+          parseInt(y[y.length - 2], 10),
+          parseInt(y[y.length - 1], 10)
+        );
       });
       return { ...map, [x]: items };
     }, {});
-    return {days, dayToItems};
+    return { days, dayToItems };
   }
 
   let days;
@@ -26,15 +31,15 @@ describe('Gilded Rose', () => {
   let store;
 
   before(() => {
-    ({days, dayToItems} = readDayToItemsFromFile());
+    ({ days, dayToItems } = readDayToItemsFromFile());
     store = new Shop(dayToItems[days[0]]);
   });
 
   it('should foo', () => {
-    for(let i = 1; i < days.length; i+=1){
+    for (let i = 1; i < days.length; i += 1) {
       const items = store.updateQuality();
+      // console.log(`day: ${days[i]}`);
       expect(items).toEqual(dayToItems[days[i]]);
     }
   });
-    
 });
